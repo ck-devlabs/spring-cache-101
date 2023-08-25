f562baa4301005681c992aa57a1478e9d4d961c0
ck-devlabs <chandrakant.devlabs@gmail.com>
2023-08-24 22:53:24 -0400

    Added common method to delete all caches

--- src/main/java/com/spring/cache/controller/SpringCacheController.java
+++ src/main/java/com/spring/cache/controller/SpringCacheController.java
@@ -4,6 +4,7 @@ import com.spring.cache.bean.Movie;
 import com.spring.cache.service.MovieService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,8 @@ public class SpringCacheController {
 
     private static final Logger LOGGER = Logger.getLogger(SpringCacheController.class.getName());
     MovieService movieService;
+
+    private CacheManager cacheManager;
 
     @GetMapping("/")
     public ResponseEntity<List<Movie>> getAllMovies(){
@@ -36,6 +39,16 @@ public class SpringCacheController {
         Movie aMovie = movieService.getMovieByTitle(movie.getTitle());
         LOGGER.log(Level.INFO, "Request getMovie Finished.....");
         return  aMovie;
+    }
+
+    @DeleteMapping("/reset")
+    public ResponseEntity<Void> clearAllCaches() {
+        cacheManager
+                .getCacheNames()
+                .stream()
+                .forEach(cacheName -> cacheManager.getCache(cacheName).clear());
+        LOGGER.info("All Cache deleted!");
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
